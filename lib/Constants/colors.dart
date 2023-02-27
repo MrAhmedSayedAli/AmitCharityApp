aff149d6eb866cfadf427b29d3e31505c3084153
Ahmed ElSayed Ali <mrahmedsayedali@gmail.com>
2023-02-27 12:20:55 +0200

    update color 👌

--- lib/Constants/colors.dart
+++ lib/Constants/colors.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color primaryBackground = Color(0xFF05AF3C);
+  static const Color primaryBackground = Color.fromRGBO(136, 193, 169,1);
   static const Color primaryText = Color(0xFF125900);
 }
